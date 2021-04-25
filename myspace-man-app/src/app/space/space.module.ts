2defd49f063794ee61752417426cadbb9ea28b18
daria.nedza <daria.nedza@kursGIT.pl>
2021-04-25 08:40:55 +0200

    nieudana praca3

--- myspace-man-app/src/app/space/space.module.ts
+++ myspace-man-app/src/app/space/space.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SpaceRoutingModule } from './space-routing.module';
 import { HangarComponent } from './hangar/hangar.component';
-import { SpaceShipComponent } from './space-ship/space-ship.component';
 
 
 @NgModule({
   declarations: [
-    HangarComponent,
-    SpaceShipComponent
-  ],
-  exports: [
     HangarComponent
   ],
+  exports:
+   [HangarComponent],
   imports: [
     CommonModule,
     SpaceRoutingModule
